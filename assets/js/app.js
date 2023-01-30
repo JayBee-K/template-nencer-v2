e809fa9d67c713069a9b636f8a566b606a32c216
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2023-01-30 11:56:36 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,24 @@
 	'use strict';
 	let windowWidth = $(window).width();
 
+	const handleStickyHeader = () => {
+		const header = $('#header');
+		const headerPosition = header.offset().top;
+		$(window).scroll(function () {
+			const scrollValue = $(window).scrollTop();
+			if (scrollValue > headerPosition) {
+				header.addClass('is-sticky');
+			} else {
+				header.removeClass('is-sticky');
+			}
+		});
+	}
+
 	$(function () {
-		$(window).resize(() => windowWidth = $(window).width());
+		handleStickyHeader();
+		$(window).resize(() => {
+			windowWidth = $(window).width();
+			handleStickyHeader();
+		});
 	});
 })(jQuery);

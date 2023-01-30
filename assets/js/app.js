21e36e96ca8f7b8b794c38d9595aa6d89d49ab35
Jay <killerdrug007@gmail.com>
2023-01-30 19:15:20 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,11 +15,42 @@
 		});
 	}
 
+	const handleNavigationMobile = () => {
+		if (windowWidth < 992) {
+			if ($("#header .header-navigation > ul > li > ul").length) {
+				$("#header .header-navigation").attr('id', 'hasMenu');
+				$("#header .header-navigation > ul > li > ul").each(function (index) {
+					$(this).prev().attr({
+						"href": "#subMenu" + index, "data-bs-toggle": "collapse"
+					});
+					$(this).attr({
+						"id": "subMenu" + index, "class": "collapse list-unstyled mb-0 header-navigation_sub--list", "data-bs-parent": "#hasMenu"
+					});
+				})
+			}
+
+			$('#call-navigation').click(function () {
+				if (!$('body').hasClass('is-navigation')) {
+					$('body').addClass('is-navigation');
+				} else {
+					$("#header .header-navigation > ul > li > .navigation-sub").collapse('hide');
+					$('body').removeClass('is-navigation');
+				}
+			});
+
+			$('#close-navigation, #header-overlay').click(function () {
+				$('body').removeClass('is-navigation');
+			});
+		}
+	}
+
 	$(function () {
 		handleStickyHeader();
+		handleNavigationMobile();
 		$(window).resize(() => {
 			windowWidth = $(window).width();
 			handleStickyHeader();
+			handleNavigationMobile();
 		});
 	});
 })(jQuery);
